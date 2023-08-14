bf14f5d53eebba67e6c80aba3e501f7df5b6a386
akinori-nakayama <green.green.0223.green@gmail.com>
2023-08-15 01:08:28 +0900

    test

---
+++ .next/static/SGdDwet1sq-1vXPqsToCB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/00a586533d958f8e.css","static/chunks/pages/index-e0f9498f2a49a2ea.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
