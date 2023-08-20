3891f8dade0913eb691b13959be2ee2d71ec5dea
akinori-nakayama <green.green.0223.green@gmail.com>
2023-08-21 04:04:20 +0900

    表示用のstateを作成、初期表示値を00にした。
    CSS調整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,6 +22,18 @@ export default function Home() {
   const [time, setTime] = useState<number>(0);
   const wasActive = useRef<boolean>(false);
 
+  // 表示用のhour, minute, second
+  const [displayHour, setDisplayHour] = useState<string>("00");
+  const [displayMinute, setDisplayMinute] = useState<string>("00");
+  const [displaySecond, setDisplaySecond] = useState<string>("00");
+
+  useEffect(()=>{
+    setDisplayHour(hour.toString().padStart(2, "0"));
+    setDisplayMinute(minute.toString().padStart(2, "0"));
+    setDisplaySecond(second.toString().padStart(2, "0"));
+  }, [hour, minute, second])
+
+  // wasActiveを更新
   useEffect(() => {
     setIsActive((prevIsActive) => {
       wasActive.current = prevIsActive;
@@ -29,14 +41,17 @@ export default function Home() {
     });
   }, [time]);
 
+  // タイマーが0になったときにアラームを鳴らす
   useEffect(() => {
     if (wasActive.current === true && isActive === false && time === 0) {
       // alert("Time's up!");
+      console.log("Time's up!");
       const audio = new Audio("/alarm.mp3");
       audio.play();
     }
   }, [isActive, time, wasActive.current]);
 
+  // タイマーのカウントダウン
   useEffect(() => {
     let interval: any = null;
     if (isActive && time > 0) {
@@ -47,16 +62,24 @@ export default function Home() {
     return () => clearInterval(interval);
   }, [isActive]);
 
+  // タイマーが0になったときにisActiveをfalseにする
   useEffect(() => {
     if (time === 0) {
       setIsActive(false);
     }
   }, [time]);
 
+  // 時間の入力を秒に変換
   useEffect(() => {
     setTime(hour * 3600 + minute * 60 + second);
   }, [hour, minute, second]);
 
+  // hour, minute, secondの初期表示を"00"にする
+  useEffect(() => {
+  }, []);
+
+
+  // timeの変更をhour, minute, secondに反映
   useEffect(() => {
     setHour(Math.floor(time / 3600));
     setMinute(Math.floor((time % 3600) / 60));
@@ -64,44 +87,56 @@ export default function Home() {
   }, [time]);
 
   return (
-    <>
-      <form
-        className={styles.time}
-        onSubmit={handleSubmit((data) => console.log(data))}
-      >
-        <input
-          type="number"
-          value={hour}
-          onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            setHour(Number(e.target.value))
-          }
-        ></input>
-        <span>:</span>
-        <input
-          type="number"
-          value={minute}
-          onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            setMinute(Number(e.target.value))
-          }
-        ></input>
-        <span>:</span>
-        <input
-          type="number"
-          value={second}
-          onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            setSecond(Number(e.target.value))
-          }
-        ></input>
-        {isActive ? (
-          <button type="button" onClick={() => setIsActive(false)}>
-            Stop
-          </button>
-        ) : (
-          <button type="button" onClick={() => setIsActive(true)}>
-            Start
-          </button>
-        )}
-      </form>
-    </>
+    <div className={styles.container}>
+      <div >
+        <form className={styles.time} onSubmit={handleSubmit((data) => console.log(data))}>
+          <input
+            className={`${styles.input} ${styles.noSpin}`}
+            type="number"
+            value={displayHour}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setHour(Number(e.target.value))
+            }
+          ></input>
+          <span>:</span>
+          <input
+            className={`${styles.input} ${styles.noSpin}`}
+            type="number"
+            value={displayMinute}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setMinute(Number(e.target.value))
+            }
+          ></input>
+          <span>:</span>
+          <input
+            className={`${styles.input} ${styles.noSpin}`}
+            type="number"
+            value={displaySecond}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setSecond(Number(e.target.value))
+            }
+          ></input>
+          <span className={styles.containerActiveButton}>
+            {isActive ? (
+              <button
+                className={styles.inactiveButton}
+                type="button"
+                onClick={() => setIsActive(false)}
+              >
+                Stop
+              </button>
+            ) : (
+              <button
+                className={styles.activeButton}
+                type="button"
+                onClick={() => setIsActive(true)}
+              >
+                Start
+              </button>
+            )}
+          </span>
+        </form>
+      </div>
+    </div>
   );
 }
