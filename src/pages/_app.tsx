6175d864fc9edcf29525b91b5276e760d264537c
akinori-nakayama <green.green.0223.green@gmail.com>
2023-10-10 02:33:00 +0900

    iOSをスクロールさせない

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,15 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
+  // iOSでスクロールさせない
+  if (typeof window !== 'undefined') {
+    document.addEventListener(
+      'touchmove',
+      function (e) {
+        e.preventDefault()
+      },
+      { passive: false }
+    )
+  }
   return <Component {...pageProps} />
 }
