bdd56216f93d5446081b281963f13ea442692ead
akinori-nakayama <green.green.0223.green@gmail.com>
2023-12-27 00:57:31 +0900

    change favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="/assets/images/favicon.svg"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/assets/images/favicon.png"
+        ></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
