7adabb4f7ab416851506638e5e1ae6bf4ace5198
Ak norinori <42559915+nakayamaakinori0@users.noreply.github.com>
2024-11-12 00:59:15 +0900

    disable scroll

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,5 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  // モバイルでスクロールさせない
-  if (typeof window !== "undefined") {
-    document.addEventListener(
-      "touchmove",
-      (e) => {
-        e.preventDefault();
-      },
-      { passive: false }
-    );
-  }
   return <Component {...pageProps} />;
 }
